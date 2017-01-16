6aaad6e25aa33c4288f3f97de6ee1c7e49e11334
Christine Dillon <christine.dillon@gmail.com>
2017-01-16 12:44:34 -0500

    Added functionality.

---
+++ commands.js
@@ -0,0 +1,79 @@
+
+var fs = require('fs');
+module.exports = {
+
+//pwd command
+pwd: function(file){
+	process.stdout.write(process.cwd());
+	process.stdout.write('\nprompt > ');
+},
+
+
+//date command
+date: function(file){
+	var output = new Date;
+	process.stdout.write(output.toString());
+	process.stdout.write('\nprompt > ');
+},
+
+ls: function(file){
+	fs.readdir('.', function(err, files) {
+  		if (err) throw err;
+  		files.forEach(function(file) {
+    	process.stdout.write(file.toString() + '\n');
+  		})
+ 	process.stdout.write('prompt > ');
+	});
+},
+
+echo: function(file){
+	var output = file.join(" ")
+	process.stdout.write(output);
+	process.stdout.write('\nprompt > ');
+},
+
+cat: function(file){
+	var fileName = './' + file;
+	fs.readFile(fileName, function(err, contents) {
+  		if (err) throw err;
+  		console.log(contents.toString())
+  		})
+ 	process.stdout.write('prompt > ');
+},
+
+head: function(file){
+
+}
+
+
+
+};
+
+
+
+
+/*pwd: process.stdin.on('data', function(data){
+	var input = data.toString().trim();
+	var output;
+	if (input ==='pwd'){
+		output = process.cwd();
+		process.stdout.write(output);
+	}
+	if (input ==='date'){
+		output = new Date;
+		process.stdout.write(output.toString());
+	}
+	process.stdout.write('\nprompt > ');
+}),
+
+
+//date command
+date: process.stdin.on('data', function(data){
+	var input = data.toString().trim();
+	var output;
+	if (input ==='date'){
+		output = new Date;
+		process.stdout.write(output.toString());
+	}
+	process.stdout.write('\nprompt > ');
+})*/
